23c99c95e1eabebf14f4a66070cd517d13721fce
Dima Statz <dima.statz@gmail.com>
2023-11-15 19:46:33 +0000

    add create_scenario

--- linga/scenario.py
+++ linga/scenario.py
@@ -10,6 +10,15 @@ class Levels(Enum):
     ADVANCED = 3
 
 
-def create_comics(topic: str, level: Levels, pages: int) -> str:
+def create_senario(topic: str, level: Levels, pages: int) -> list:
+    """creates scenario by using chat_gpt and returns list of pages"""
+    prompt = f"{topic} {level} {pages}"
+    assert prompt
+    return []
+
+
+def create_comics(topic: str, level: Levels, pages: int) -> list:
     """creates comics with"""
+    pages_list = create_senario(topic, level, pages)
+    assert len(pages_list) == 0
     return f"{topic} {level} {pages}"
