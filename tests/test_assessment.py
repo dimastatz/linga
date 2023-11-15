79a54dd97b6024de09f2bfd089922fd4039ab0f9
Yuri Ronin <yuri.ronin@verizonmedia.com>
2023-11-15 12:28:25 +0200

    minor test corerction

--- tests/test_assessment.py
+++ tests/test_assessment.py
@@ -6,13 +6,13 @@ from linga.scenario import Levels
 def test_create_assessment_prompt_for_beginner():
     """tests create assessment"""
     level = Levels.BEGINNER
-    language = "Russian"
+    language = "Hebrew"
     comics = (
         "Yuri Alekseyevich Gagarin was a Soviet pilot and cosmonaut who, aboard the first "
         "successful crewed spaceflight, became the first human to journey into outer space. "
         "Travelling on Vostok 1, he orbited Earth at an altitude of 327 kilometres for 108 minutes"
     )
-    res = create_assessment_prompt(comics, "", level, "Russian")
+    res = create_assessment_prompt(comics, "", level, language)
 
     assert comics in res
     assert "chat history" not in res
@@ -29,7 +29,7 @@ def test_create_assessment_prompt_for_beginner_with_history():
         "successful crewed spaceflight, became the first human to journey into outer space. "
         "Travelling on Vostok 1, he orbited Earth at an altitude of 327 kilometres for 108 minutes"
     )
-    res = create_assessment_prompt(comics, "What is cosmonaut?", level, "Russian")
+    res = create_assessment_prompt(comics, "What is cosmonaut?", level, language)
 
     assert comics in res
     assert "chat history" in res
