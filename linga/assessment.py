64afafa84dfff53579d3a6d64a634817c1af1c81
Yuri Ronin <yuri.ronin@verizonmedia.com>
2023-11-15 12:20:04 +0200

    fixing lint and adding more tests

--- linga/assessment.py
+++ linga/assessment.py
@@ -1,17 +1,22 @@
+"""Assessment module."""
+
 from linga.scenario import Levels
 
 
-def create_assessment_prompt(comics_text: str, chat_history: str, level: Levels, native_language: str) -> str:
+def create_assessment_prompt(
+    comics_text: str, chat_history: str, level: Levels, native_language: str
+) -> str:
     """creates assessment prompt"""
     requested_questions_count = 10
-    prompt = f"You are an English teacher, teaching foreign students, who speak {native_language}. " \
-             f"You gave them comics to read. Here is the comics text {comics_text} that you need to prepare multiple " \
-             f"choice questions for to check how well students learned the new English words from the comics. " \
-             f"Write {requested_questions_count} questions in {native_language} and send them to me. " \
-             f"Make sure to fit the questions for {level.name} level." \
-             f"Use the following format: 'question' 'answer1' 'answer2' 'answer3' 'answer4' 'correct answer' " \
-             f"for each question and return a json in the following schema]" \
-             """{
+    prompt = (
+        f"You are an English teacher, teaching foreign students, who speak {native_language}. "
+        f"You gave them comics to read. Here is the comics text {comics_text} that you need to "
+        f"prepare multiple choice questions for to check how well students learned the new "
+        f"English words from the comics. Write {requested_questions_count} questions in "
+        f"{native_language} and send them to me. Make sure to fit the questions for {level.name} "
+        f"level. Use the following format: 'question' 'answer1' 'answer2' 'answer3' 'answer4' "
+        f"'correct answer' for each question and return a json in the following schema]"
+        """{
              "$schema": "http://json-schema.org/draft-04/schema#",
               "type": "array",
               "items": [
@@ -48,11 +53,14 @@ def create_assessment_prompt(comics_text: str, chat_history: str, level: Levels,
                 }
               ]
             }"""
+    )
 
     if chat_history:
-        prompt += f"" \
-                  f"Use the following chat history {chat_history} to see what questions were already asked. " \
-                  f"Repeat one of questions that was repeated less than others and that user gave correct answer for." \
-                  f"Also repeat one question that user answered incorrectly."
+        prompt += (
+            f""
+            f"Use the following chat history {chat_history} to see what questions were already "
+            f"asked. Repeat one of questions that was repeated less than others and that user "
+            f"gave correct answer for. Also repeat one question that user answered incorrectly."
+        )
 
     return prompt
--- tests/test_assessment.py
+++ tests/test_assessment.py
@@ -7,16 +7,31 @@ def test_create_assessment_prompt_for_beginner():
     """tests create assessment"""
     level = Levels.BEGINNER
     language = "Russian"
-    comics = "Yuri Alekseyevich Gagarin was a Soviet pilot and cosmonaut who, aboard the first successful crewed " \
-             "spaceflight, became the first human to journey into outer space. Travelling on Vostok 1, he orbited " \
-             "Earth at an altitude of 327 kilometres for 108 minutes."
-    res = create_assessment_prompt(
-        comics,
-        "",
-        level.name, "Russian")
-    print(res)
+    comics = (
+        "Yuri Alekseyevich Gagarin was a Soviet pilot and cosmonaut who, aboard the first "
+        "successful crewed spaceflight, became the first human to journey into outer space. "
+        "Travelling on Vostok 1, he orbited Earth at an altitude of 327 kilometres for 108 minutes"
+    )
+    res = create_assessment_prompt(comics, "", level, "Russian")
 
     assert comics in res
     assert "chat history" not in res
+    assert level.name in res
+    assert language in res
+
+
+def test_create_assessment_prompt_for_beginner_with_history():
+    """tests create assessment"""
+    level = Levels.BEGINNER
+    language = "Russian"
+    comics = (
+        "Yuri Alekseyevich Gagarin was a Soviet pilot and cosmonaut who, aboard the first "
+        "successful crewed spaceflight, became the first human to journey into outer space. "
+        "Travelling on Vostok 1, he orbited Earth at an altitude of 327 kilometres for 108 minutes"
+    )
+    res = create_assessment_prompt(comics, "What is cosmonaut?", level, "Russian")
+
+    assert comics in res
+    assert "chat history" in res
     assert level.name in res
     assert language in res
