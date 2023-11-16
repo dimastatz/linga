4cd76412a9021664bd5eaf9e2e608177eb64f83c
Dima Statz <dima.statz@gmail.com>
2023-11-16 19:52:29 +0000

    add scenario

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -1,8 +1,15 @@
 """ test scenario module """
+from linga.scenario import create_senario
 from linga.scenario import create_comics, Levels
 
 
-def test_index():
-    """tests basic flow"""
+def test_create_comics():
+    """tests basic comics"""
     res = create_comics("history", Levels.BEGINNER, 7)
     assert res == "history Levels.BEGINNER 7"
+
+
+def test_create_scenario():
+    """tests basic script"""
+    scenario = create_senario("history", Levels.BEGINNER, 7)
+    assert scenario == []
