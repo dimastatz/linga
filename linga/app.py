e62da2957cd442cd8916a28afb1fb6901e3a28fa
Dima Statz <dima.statz@gmail.com>
2023-11-25 19:50:41 +0000

    add canvas

--- linga/app.py
+++ linga/app.py
@@ -14,26 +14,6 @@ st.header("pdf-GPT")
 logger.info("header set")
 
 
-def clear_submit():
-    """submit = false"""
-    st.session_state["submit"] = False
-
-
-def show_pdf(file_path):
-    """load pdf content"""
-    with open(file_path, "rb") as file:
-        base64_pdf = base64.b64encode(file.read()).decode("utf-8")
-        logger.info("pdf is loaded {len(base64_pdf)}")
-
-    pdf_display = (
-        f'<iframe src="data:application/pdf;base64,{base64_pdf}"'
-        + 'width="800" height="800" type="application/pdf"></iframe>'
-    )
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-show_pdf("./docs/comics.pdf")
-
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
 )
@@ -45,7 +25,7 @@ stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
+#realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
 Canvas = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
@@ -53,13 +33,10 @@ Canvas = st_canvas(
     stroke_color=stroke_color,
     background_color=bg_color,
     background_image=Image.open(bg_image) if bg_image else None,
-    update_streamlit=realtime_update,
-    height=150,
+    #update_streamlit=realtime_update,
+    height=600,
     drawing_mode=drawing_mode,
     point_display_radius=point_display_radius if drawing_mode == "point" else 0,
     key="canvas",
 )
 
-# Do something interesting with the image data and paths
-if Canvas.image_data is not None:
-    st.image(Canvas.image_data)
