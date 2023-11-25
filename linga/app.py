29df9d155b33694045c74c733213f50211e374e9
Dima Statz <dima.statz@gmail.com>
2023-11-25 19:37:46 +0000

    add canvas

--- linga/app.py
+++ linga/app.py
@@ -1,8 +1,10 @@
 """ test pdf reader """
 
 import base64
+from PIL import Image
 import streamlit as st
 from streamlit.logger import get_logger
+from streamlit_drawable_canvas import st_canvas
 
 
 logger = get_logger(__name__)
@@ -31,3 +33,33 @@ def show_pdf(file_path):
 
 
 show_pdf("./docs/comics.pdf")
+
+drawing_mode = st.sidebar.selectbox(
+    "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
+)
+
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+if drawing_mode == "point":
+    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+
+realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
+Canvas = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    background_image=Image.open(bg_image) if bg_image else None,
+    update_streamlit=realtime_update,
+    height=150,
+    drawing_mode=drawing_mode,
+    point_display_radius=point_display_radius if drawing_mode == "point" else 0,
+    key="canvas",
+)
+
+# Do something interesting with the image data and paths
+if Canvas.image_data is not None:
+    st.image(Canvas.image_data)
