78371e5bcbbf216bbed72213dcd73d46dd58d238
Dima Statz <dima.statz@gmail.com>
2024-01-07 20:01:52 +0000

    style

--- linga/app.py
+++ linga/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__, "/static")
 
 @app.route("/")
 def get_canvas():
-    """return static content"""
+    """ return static content """
     return app.send_static_file("index.html")
