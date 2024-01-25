7ca6c25b690804cd23ed2a27236294aac1103bcd
Dima Statz <dima.statz@gmail.com>
2024-01-25 20:31:07 +0000

    fix lint

--- linga/app.py
+++ linga/app.py
@@ -1,7 +1,8 @@
 """ flask app """
+import os
 import logging
+import tempfile
 import traceback
-import tempfile, os
 from flask import Flask, request
 from flask_socketio import SocketIO, emit, join_room
 from linga.scenario import run_transcribe
@@ -20,34 +21,44 @@ def get_canvas():
 def transcribe():
     """return static content"""
     try:
-        file = request.files['file']
-        filed, path = tempfile.mkstemp()
-        logging.info(f"file path{filed}, {path}")
+        file = request.files["file"]
+        _, path = tempfile.mkstemp()
+        logging.info("file path %s", path)
         file.save(path)
 
         text = run_transcribe(path), 200
         os.remove(path)
         return text
-    except Exception as e:
+    except Exception as error: # pylint: disable=W0718
         logging.error(traceback.format_exc())
-        return f"Transcription failed {e}", 500
+        return f"Transcription failed {error}", 500
 
 
-@socketio.on('join')
+# pylint: disable=E0602
+@socketio.on("join") 
 def join(message):
     """join chat room"""
-    username = message['username']
-    room = message['room']
+    username = message["username"]
+    room = message["room"]
     join_room(room)
-    print('RoomEvent: {} has joined the room {}\n'.format(username, room))
-    emit('ready', {username: username}, to=room, skip_sid=request.sid)
+
+    logging.info("RoomEvent: %s has joined the room %s", username, room)
+    emit("ready", {username: username}, to=room, skip_sid=request.sid)
 
 
-@socketio.on('data')
+@socketio.on("data")
 def transfer_data(message):
     """transfer message"""
-    username = message['username']
-    room = message['room']
-    data = message['data']
-    print('DataEvent: {} has sent the data:\n {}\n'.format(username, data))
-    emit('data', data, to=room, skip_sid=request.sid)
+    username = message["username"]
+    room = message["room"]
+    data = message["data"]
+
+    logging.info("RoomEvent: %s has joined the room %s", username, data)
+    emit("data", data, to=room, skip_sid=request.sid)
+
+
+@socketio.on_error_default
+def default_error_handler(error):
+    """error handler"""
+    logging.info("Error: %s", error)
+    socketio.stop()
